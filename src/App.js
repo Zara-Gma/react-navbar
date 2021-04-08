fe43b57b5eb7ce7aa4458451b3685390400e082a
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-07 22:14:08 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Services from "./pages/services";
+import ContactUs from "./pages/contactUs";
+import SignUp from "./pages/signUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/sign-up" exact component={SignUp} />
+      </Switch>
+    </Router>
   );
 }
 
